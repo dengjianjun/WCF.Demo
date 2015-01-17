63226f95aa83cc0943063b4de340b191fec39faf
dengjianjun <969234280@qq.com>
2015-01-17 22:34:50 +0800

    修改binding

--- WCF.Demo/Program.cs
+++ WCF.Demo/Program.cs
@@ -12,13 +12,15 @@ namespace WCF.Demo
     {
         static void Main(string[] args)
         {
-            //客户端访问有多种方式，此处只显示一种
-            //利用ChannelFactory的CreateChannel方法创建一个IData的代理对象，其中参数“DataService”就是刚才在App.config中定义的endpoint的名称
-            var proxy = new ChannelFactory<Server.IData>("DataService").CreateChannel();
-            //调用SayHello方法
-            Console.WriteLine(proxy.SayHello("WCF"));
-            //用完后一定要关闭，因为服务端有最大连接数，不关闭会在一定时间内一直占着有效连接
-            ((IChannel)proxy).Close(); 
+            //定义一个http方式的代理，配置使用httpDataService中的定义 
+            var httpProxy = new ChannelFactory<Server.IData>("httpDataService").CreateChannel();
+            Console.WriteLine(httpProxy.SayHello("WCF"));
+            ((IChannel)httpProxy).Close();
+
+            //定义一个tcp方式的代理，配置使用tcpDataService中的定义 
+            var tcpProxy = new ChannelFactory<Server.IData>("tcpDataService").CreateChannel();
+            Console.WriteLine(tcpProxy.SayHello("WCF"));
+            ((IChannel)tcpProxy).Close();
             Console.ReadKey();
         }
     }
